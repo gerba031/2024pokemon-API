b6600bcec198f71ac536c22e462346020a323437
Ivan Novokmet <gerba.dev@gmail.com>
2024-05-31 18:35:06 +0200

    added template to html and implemented handlebars

---
+++ pokemon-script.js
@@ -0,0 +1,23 @@
+Handlebars.registerHelper("increaseIndex", function (inValue) {
+    return ++inValue
+})
+
+let xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://pokeapi.co/api/v2/pokemon-color/yellow", true);
+
+function popuniPokemone() {
+    const data = JSON.parse(xhr.response);
+    const source = document.getElementById("pokemon-lista").innerHTML;
+    const template = Handlebars.compile(source);
+    const context = { pokemon: data.pokemon_species.slice(0, 20) };
+    const html = template(context);
+
+    document.getElementById("pokemon-output").innerHTML = html;
+}
+
+xhr.onload = function () {
+    popuniPokemone();
+}
+
+xhr.send();
