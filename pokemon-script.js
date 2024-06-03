df3b2b8b6e5609f19f7c89209ebda73c344014ad
Ivan Novokmet <gerba.dev@gmail.com>
2024-06-03 18:37:37 +0200

    uređena tablica s pokemonima

--- pokemon-script.js
+++ pokemon-script.js
@@ -16,10 +16,42 @@ function popuniPokemone() {
     document.getElementById("pokemon-output").innerHTML = html;
 }
 
+function afterRender() {
+    $('table th').css('color, darkBlue');
+    // drugi način dodavanja stila 
+    // $("table th".addClass('headerStyle'));
+
+    // u tablici svaki drugi redak obojati drugom bojom
+    $("table tr:nth-child(even)").addClass("zebra");
+    //obrisati sve koji počinju slovom "p"
+    setTimeout(function () {
+        const aTag = $("table td a:contains('p')").filter(function () {
+            return this.innerHTML.indexOf("p") == 0;
+        });
+        //remove closest "tr" element
+        aTag.closest("tr").remove();
+    }, 2000);
+
+    $("table tr:nth-child(even)").removeClass("zebra");
+
+    $("table tr:nth-child(even)").addClass("zebra");
+
+}
+
+
+
+
 xhr.onload = function () {
+    //napravi tablicu  pokemona
+
     popuniPokemone();
+    //altiviraj popover
 
     $('[data-bs-toggle="popover"]').popover();
+    // zebra uzorak
+
+    afterRender();
+
 };
 
 xhr.send();
